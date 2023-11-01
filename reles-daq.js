471f3facfac485086633b7a5a9b8279ce917c73b
Lucas Kroth <lucas.kroth@inova.ind.br>
2023-11-01 16:45:44 -0300

    otimização de tempo em 100x no acionamento de reles - nova documentacao via intelisense

--- reles-daq.js
+++ reles-daq.js
@@ -1,71 +1,108 @@
 ﻿class DAQRelay {
 
+    static State = []
+
     /** 
-     * Formato esperado [Array's] : [1, 2, 5] 
-     * 
      * @param {array} toTurnOnRelays
      * @param {number} delay
+     * 
+     * # Formato Esperado
+     * 
+     * ```js
+     * toTurnOnRelays = [1, 2, 3]
+     * delay = 1000
+     * ```
+     * 
+     * # Exemplos
+     * 
+     * Utilizando buffer interno `DAQRelay.State` e sem timeout:
+     * ```js
+     * DAQRelay.TurnOn()
+     * ```
+     * 
+     * Utilizando buffer interno com timeout:
+     * ```js
+     * await DAQRelay.TurnOn(undefined, 1000)
+     * ```
+     * 
+     * Utilizando outro buffer com timeout:
+     * ```js
+     * await DAQRelay.TurnOn(myBuffer, 1000)
+     * ```
      */
-    static async TurnOn(toTurnOnRelays, delay = 100) {
+    static async TurnOn(toTurnOnRelays = DAQRelay.State, delay = 0) {
+        let daqRelays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
 
-        return new Promise((resolve) => {
-
-            let daqRelays = []
-            let toTurnOffRelays = []
-
-            /**
-             * Monta lista de reles do DAQ
-             */
-            for (let relay = 1; relay <= 18; relay++) {
-                daqRelays.push(relay)
+        //Desaciona relés não inclusos no buffer
+        for (const relay of daqRelays) {
+            if (!toTurnOnRelays.includes(relay)) {
+                DAQ.runInstruction("desliga_rele", ["RL" + relay], () => { }, pvi.daq.getQueryID())
             }
+        }
 
-            /**
-             * Seleciona reles para desacionamento
-             */
-            daqRelays.forEach(relay => {
-                if (!toTurnOnRelays.includes(relay)) {
-                    toTurnOffRelays.push(relay)
-                }
-            })
+        //Aciona reles inclusos no buffer
+        for (const relay of toTurnOnRelays) {
+            DAQ.runInstruction("liga_rele", ["RL" + relay], () => { }, pvi.daq.getQueryID())
+        }
 
-            /**
-             * Desaciona reles selecionados
-             */
-            toTurnOffRelays.forEach(relay => {
-                pvi.daq.desligaRele(relay)
-            })
+        console.warn("Relés Acionados", toTurnOnRelays)
 
-            /**
-             * Aciona reles passados inicialmente
-             */
-            toTurnOnRelays.forEach(relay => {
-                pvi.daq.ligaRele(relay)
-            })
-
-            console.log("Relés Acionados", toTurnOnRelays)
-
-            setTimeout(() => {
-                resolve()
-            }, delay)
-        })
+        if (delay) { await this.Delay(delay) }
+        return
     }
 
     /**
-     * @param {number} relay 
-     * @param {[number]} buffer 
+     * @param {Number} relay 
+     * @param {Array} buffer
+     * 
+     * # Formato Esperado
+     * 
+     * ```js
+     * relay = 4
+     * buffer = [1, 2, 3]
+     * ```
+     * 
+     * # Exemplos
+     * 
+     * Adicionando relé ao buffer interno `DAQRelay.State`:
+     * ```js
+     * DAQRelay.AddRelay(4)
+     * ```
+     * 
+     * Adicionando relé à outro buffer:
+     * ```js
+     * DAQRelay.AddRelay(buffer, 1000)
+     * ```
      */
-    static AddRelay(relay, buffer) {
+    static AddRelay(relay, buffer = DAQRelay.State) {
         if (!buffer.includes(relay)) {
             buffer.push(relay)
         }
     }
 
     /**
-     * @param {number} relay 
-     * @param {[number]} buffer 
+     * @param {Number} relay 
+     * @param {Array} buffer
+     * # Formato Esperado
+     * 
+     * ```js
+     * relay = 1
+     * buffer = [1, 2, 3]
+     * ```
+     * 
+     * # Exemplos
+     * 
+     * Removendo relé ao buffer interno `DAQRelay.State`:
+     * ```js
+     * DAQRelay.RemoveRelay(1)
+     * ```
+     * 
+     * Removendo relé à outro buffer:
+     * ```js
+     * DAQRelay.RemoveRelay(buffer, 1000)
+     * ```
      */
-    static RemoveRelay(relay, buffer) {
+    static RemoveRelay(relay, buffer = DAQRelay.State) {
         buffer.forEach((bufferRelay, index) => {
             if (bufferRelay == relay) {
                 buffer.splice(index, 1)
@@ -74,9 +111,132 @@
     }
 
     /**
-     * @param {[number]} buffer 
+     * @param {Array} buffer
+     * # Formato Esperado
+     * 
+     * ```js
+     * buffer = [1, 2, 3]
+     * ```
+     * 
+     * # Exemplo
+     * 
+     * ```js
+     * DAQRelay.ClearBuffer(myBuffer)
+     * ```
      */
-    static ClearBuffer(buffer) {
+    static ClearBuffer(buffer = DAQRelay.State) {
         buffer.splice(0, buffer.length)
+    }
+
+    static async Delay(timeout = 1000) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve()
+            }, timeout)
+        })
+    }
+}
+
+class Power {
+
+    /**
+     * @param {Number} voltage 
+     * @param {Number} delay 
+     * 
+     * # Exemplo
+     * 
+     * Sem delay pós-energização:
+     * ```js
+     * Power.On(220)
+     * ```
+     * 
+     * Com delay pós-energização:
+     * ```js
+     * await Power.On(220, 2000)
+     * ```
+     * ### Opções de tensão
+     * 
+     * `12`
+     * `24`
+     * `110`
+     * `220`
+     */
+    static async On(voltage, delay = 0) {
+        switch (voltage) {
+            case 12:
+                Log.console(`12VDC Alimentação Geral [Delay ${delay}ms]`, Log.Colors.Yellow.Gold)
+                pvi.daq.alimenta12()
+                break
+            case 24:
+                Log.console(`24VDC Alimentação Geral [Delay ${delay}ms]`, Log.Colors.Yellow.Gold)
+                pvi.daq.alimenta24()
+                break
+            case 110:
+                Log.console(`110VAC Alimentação Geral [Delay ${delay}ms]`, Log.Colors.Yellow.Gold)
+                pvi.daq.alimenta110()
+                break
+            case 220:
+                Log.console(`220VAC Alimentação Geral [Delay ${delay}ms]`, Log.Colors.Yellow.Gold)
+                pvi.daq.alimenta220()
+                break
+            default:
+                alert("Tensão informada inválida")
+                break
+        }
+        if (delay) { await this.Delay(delay) }
+        return
+    }
+
+    /**
+     * 
+     * @param {Number} delay
+     * # Exemplo
+     * 
+     * Sem delay pós-energização:
+     * ```js
+     * Power.Off()
+     * ```
+     * 
+     * Com delay pós-energização:
+     * ```js
+     * await Power.Off(2000)
+     * ```
+     */
+    static async Off(delay = 0) {
+        Log.console(`Desenergizando Alimentação Geral [Delay ${delay}ms]`, Log.Colors.Yellow.Gold)
+        pvi.daq.desligaAlimentacao()
+        if (delay) { await this.Delay(delay) }
+        return
+    }
+
+    /**
+     * 
+     * @param {Boolean} state 
+     * @param {Number} delay 
+     * # Exemplo
+     * 
+     * Sem delay pós-energização:
+     * ```js
+     * Power.Aux(true)
+     * ```
+     * 
+     * Sem delay pós-energização:
+     * ```js
+     * await Power.Aux(false, 1000)
+     * ```
+     */
+    static async Aux(state, delay = 0) {
+        Log.console(`${state ? "Energizando" : "Desenergizando"} Alimentação Auxiliar [Delay ${delay}ms]`, Log.Colors.Yellow.Gold)
+        state ? pvi.daq.ligaAux220() : pvi.daq.desligaAux220()
+        if (delay) { await this.Delay(delay) }
+        return
+    }
+
+    static async Delay(timeout = 1000) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve()
+            }, timeout)
+        })
     }
 }
